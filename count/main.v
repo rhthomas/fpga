f240dbf984262d04811625caa84ced449e1295da
Rhys Thomas <rhys97@me.com>
2018-05-15 21:51:04 +0100

    Upload counter example.

---
+++ count/main.v
@@ -0,0 +1,27 @@
+module counter (
+    input clk_in,
+    output [4:0] value
+);
+
+    reg [26:0] counter;
+
+    always @(posedge clk_in)
+        counter <= counter + 1;
+
+    assign value = counter[26:22];
+
+endmodule
+
+
+
+module main (
+    input CLK,
+    output [4:0] LED
+);
+
+    counter c (
+        .clk_in(CLK),
+        .value(LED)
+    );
+
+endmodule
